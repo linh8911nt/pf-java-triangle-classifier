991cc60293580c2b044e6eb542eb9aa43509e885
linh8911nt <thanhlinh12314@gmail.com>
2018-03-27 10:12:31 +0700

    pf-java-triangle-classifier

---
+++ src/TriangleClassifier.java
@@ -0,0 +1,25 @@
+public class TriangleClassifier {
+//    public static boolean normalTriangle(int edgeA, int edgeB, int edgeC){
+//        if ((edgeA + edgeB) > edgeC || )
+//    }
+    public static String triangleClassifier(int edgeA, int edgeB, int edgeC){
+        String result = "";
+        if ((edgeA + edgeB) > edgeC || (edgeA + edgeC) > edgeB || (edgeB + edgeC) > edgeA)
+            result = "normalTriangle";
+
+        if ((edgeA + edgeB) < edgeC || (edgeA + edgeC) < edgeB || (edgeB + edgeC) < edgeA)
+            result = "notTriangles";
+
+        if (edgeA == edgeB && edgeA == edgeC && edgeB == edgeC)
+            result = "equilateralTriangle";
+
+        else if (edgeA == edgeB || edgeA == edgeC || edgeB == edgeC)
+            result = "isoscelesTriangle";
+
+        if (edgeA <= 0 || edgeB <= 0 || edgeC <= 0)
+            result = "notTriangles";
+
+        return result;
+    }
+
+}
